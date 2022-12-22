9cb0835a5a8398de7a9c80d458801d8df896c4b2
Nazarbek2007 <87320583+Nazarbek2007@users.noreply.github.com>
2022-12-22 16:57:50 +0500

    addd

---
+++ js-dz1/page.js
@@ -0,0 +1,20 @@
+var Name = prompt('Введите свое имя');
+console.log('Вас зовут ' + Name);
+var age = prompt("Введите свой возраст");
+console.log('Вам ' + age);
+
+
+var plus = +prompt('Пример на плюс: 25 + 5');
+console.log('ответ: 25 + 5 = 30 Ваш ответ: ' + plus);
+
+var minus = +prompt('Пример на плюс: 25 - 5');
+console.log('ответ: 25 - 5 = 20 Ваш ответ: ' + minus);
+
+var umno = +prompt('Пример на плюс: 25 * 5');
+console.log('ответ: 25 * 5 = 125 Ваш ответ: ' + umno);
+
+var del = +prompt('Пример на плюс: 25 / 5');
+console.log('ответ: 25 / 5 = 5 Ваш ответ: ' + del);
+
+var prots = +prompt('Пример на плюс: 25 % 5');
+console.log('ответ: 25 % 4 = 1 Ваш ответ: ' + prots);
